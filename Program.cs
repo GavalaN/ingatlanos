fe3d3d9fd61507b2e234c65682b4011ccafe2079
TMarcell <82504134+TMarcellB@users.noreply.github.com>
2023-05-18 13:41:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -9,13 +11,50 @@ namespace ConsoleApp1
         public int alap;
         public int ar;
     }
+    struct ugyfel{
+        public int azon;
+        public string nev;
+        public int telefon;
+    }
     class Program
     {
+        static List<ingatlan> Ingatlan(List<ingatlan> lista)
+        {
+            ingatlan sor;
+            StreamReader sr = new StreamReader(@"ingatlan.txt" + Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] temp = sr.ReadLine().Split('\t');
+                sor.id = int.Parse(temp[0]);
+                sor.cim = temp[1];
+                sor.alap = int.Parse(temp[2]);
+                sor.ar = int.Parse(temp[3]);
+                lista.Add(sor);
+            }
+            sr.Close();
+            return lista;
+        }
+        static List<ugyfel> Ugyfel(List<ugyfel> lista)
+        {
+            ugyfel sor;
+            StreamReader sr = new StreamReader(@"ugyfel.txt" + Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] temp = sr.ReadLine().Split('\t');
+                sor.azon = int.Parse(temp[0]);
+                sor.nev = temp[1];
+                sor.telefon = int.Parse(temp[2]);
+                lista.Add(sor);
+            }
+            sr.Close();
+            return lista;
 
-        
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<ingatlan> uj =new List<ingatlan>();
+            uj = Ingatlan(uj);
         }
     }
 }
